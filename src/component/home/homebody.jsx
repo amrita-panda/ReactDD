658ffbaf1e7cde76483a8405f97108e214260aab
Amrita panda <apanda@digitaldots.io>
2020-05-07 14:56:05 +0530

    "Dummy check"

--- src/component/home/homebody.jsx
+++ src/component/home/homebody.jsx
@@ -38,6 +38,12 @@ const HomePageBody=()=>(
                     <h2 className='title'>Glas sess</h2>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
+            </div> 
+            <div className='menu-item'>
+                <div className='content'>
+                    <h2 className='title'>Dresses</h2>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
             </div>   
         </div>
 
